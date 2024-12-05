388fa0a72c0e6a372d206ef3a0db54ab6b7633a0
jshn9515 <jshn9515@163.com>
2024-12-05 19:37:57 +0800

    Improve & revise code to improve readability and execution speed

--- torchkan/kan_linear/utils.py
+++ torchkan/kan_linear/utils.py
@@ -1,5 +1,7 @@
 import torch
+from torch import Tensor
 import torch.nn as nn
+from typing import Optional
 
 
 class RadialBasisFunction(nn.Module):
@@ -8,12 +10,12 @@ class RadialBasisFunction(nn.Module):
             grid_min: float = -2.,
             grid_max: float = 2.,
             num_grids: int = 8,
-            denominator: float = None,  # larger denominators lead to smoother basis
+            denominator: Optional[float] = None  # larger denominators lead to smoother basis
     ):
         super().__init__()
         grid = torch.linspace(grid_min, grid_max, num_grids)
         self.grid = torch.nn.Parameter(grid, requires_grad=False)
         self.denominator = denominator or (grid_max - grid_min) / (num_grids - 1)
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
         return torch.exp(-((x[..., None] - self.grid) / self.denominator) ** 2)
