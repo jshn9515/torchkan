0f9b6d17712153267a928e87278f2e1d13bc9585
jshn9515 <jshn9510@gmail.com>
2025-04-04 15:30:56 +0800

    Upload entire project

---
+++ torchkan/conv/__init__.py
@@ -0,0 +1,51 @@
+# ruff: noqa: I001
+from .fast_kan_conv import (
+    FastKANConv1DLayer as FastKANConv1DLayer,
+    FastKANConv2DLayer as FastKANConv2DLayer,
+    FastKANConv3DLayer as FastKANConv3DLayer,
+)
+from .kabn_conv import (
+    KABNConv1DLayer as KABNConv1DLayer,
+    KABNConv2DLayer as KABNConv2DLayer,
+    KABNConv3DLayer as KABNConv3DLayer,
+)
+from .kacn_conv import (
+    KACNConv1DLayer as KACNConv1DLayer,
+    KACNConv2DLayer as KACNConv2DLayer,
+    KACNConv3DLayer as KACNConv3DLayer,
+)
+from .kagn_conv import (
+    KAGNConv1DLayer as KAGNConv1DLayer,
+    KAGNConv2DLayer as KAGNConv2DLayer,
+    KAGNConv3DLayer as KAGNConv3DLayer,
+)
+from .kajn_conv import (
+    KAJNConv1DLayer as KAJNConv1DLayer,
+    KAJNConv2DLayer as KAJNConv2DLayer,
+    KAJNConv3DLayer as KAJNConv3DLayer,
+)
+from .kaln_conv import (
+    KALNConv1DLayer as KALNConv1DLayer,
+    KALNConv2DLayer as KALNConv2DLayer,
+    KALNConv3DLayer as KALNConv3DLayer,
+)
+from .kan_conv import (
+    KANConv1DLayer as KANConv1DLayer,
+    KANConv2DLayer as KANConv2DLayer,
+    KANConv3DLayer as KANConv3DLayer,
+)
+from .relukan_bottleneck_conv import (
+    BottleNeckReLUKANConv1DLayer as BottleNeckReLUKANConv1DLayer,
+    BottleNeckReLUKANConv2DLayer as BottleNeckReLUKANConv2DLayer,
+    BottleNeckReLUKANConv3DLayer as BottleNeckReLUKANConv3DLayer,
+)
+from .relukan_conv import (
+    ReLUKANConv1DLayer as ReLUKANConv1DLayer,
+    ReLUKANConv2DLayer as ReLUKANConv2DLayer,
+    ReLUKANConv3DLayer as ReLUKANConv3DLayer,
+)
+from .wav_kan import (
+    WavKANConv1DLayer as WavKANConv1DLayer,
+    WavKANConv2DLayer as WavKANConv2DLayer,
+    WavKANConv3DLayer as WavKANConv3DLayer,
+)
---
+++ torchkan/conv/kagn_conv_v2.py
@@ -0,0 +1,253 @@
+# Based on this: https://github.com/Khochawongwat/GRAMKAN/blob/main/model.py
+
+from functools import lru_cache
+from typing import Callable, Literal
+
+import torch
+import torch.nn as nn
+from torch import Tensor
+from torch.nn.functional import conv1d, conv2d, conv3d
+
+PaddingType = Literal['valid', 'same']
+
+ 
+class KAGNConvNDLayerV2(nn.Module):
+    def __init__(
+        self,
+        conv_class: type[nn.Module],
+        norm_class: type[nn.Module],
+        conv_w_fun: Callable,
+        ndim: int,
+        in_channels: int,
+        out_channels: int,
+        degree: int,
+        kernel_size: int | tuple[int, ...],
+        stride: int | tuple[int, ...],
+        padding: PaddingType | int | tuple[int, ...],
+        dilation: int | tuple[int, ...],
+        groups: int = 1,
+        base_activation: Callable[[Tensor], Tensor] = nn.SiLU(),
+        dropout: float = 0.0,
+        **norm_kwargs,
+    ):
+        super(KAGNConvNDLayerV2, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.degree = degree
+        self.kernel_size = kernel_size
+        self.padding = padding
+        self.stride = stride
+        self.dilation = dilation
+        self.groups = groups
+        self.base_activation = base_activation
+        self.conv_w_fun = conv_w_fun
+        self.ndim = ndim
+        self.dropout = None
+        self.norm_kwargs = norm_kwargs
+
+        if dropout > 0:
+            if ndim == 1:
+                self.dropout = nn.Dropout1d(p=dropout)
+            if ndim == 2:
+                self.dropout = nn.Dropout2d(p=dropout)
+            if ndim == 3:
+                self.dropout = nn.Dropout3d(p=dropout)
+
+        if groups <= 0:
+            raise ValueError('groups must be a positive integer')
+        if in_channels % groups != 0:
+            raise ValueError('input_dim must be divisible by groups')
+        if out_channels % groups != 0:
+            raise ValueError('output_dim must be divisible by groups')
+
+        self.base_conv = conv_class(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            groups=1,
+            bias=False,
+        )
+
+        self.poly_conv = conv_class(
+            in_channels=in_channels * (degree + 1),
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            groups=1,
+            bias=False,
+        )
+
+        self.layer_norm = norm_class(out_channels, **norm_kwargs)
+        self.beta_weights = nn.Parameter(torch.zeros(degree + 1, dtype=torch.float32))
+
+        # Initialize weights using Kaiming uniform distribution for better training start
+        # for conv_layer in self.base_conv:
+        nn.init.kaiming_uniform_(
+            torch.as_tensor(self.base_conv.weight), nonlinearity='linear'
+        )
+        nn.init.kaiming_uniform_(
+            torch.as_tensor(self.poly_conv.weight), nonlinearity='linear'
+        )
+
+        if isinstance(kernel_size, int):
+            kernel_size = (kernel_size,)
+
+        nn.init.normal_(
+            self.beta_weights,
+            mean=0.0,
+            std=1.0
+            / (
+                (sum(kernel_size) / len(kernel_size)) ** ndim
+                * self.in_channels
+                * (self.degree + 1.0)
+            ),
+        )
+
+    def beta(self, n: int, m: int) -> Tensor:
+        return (
+            ((m + n) * (m - n) * n**2) / (m**2 / (4.0 * n**2 - 1.0))
+        ) * self.beta_weights[n]
+
+    @lru_cache(maxsize=128)  # Cache to avoid recomputation of Gram polynomials
+    def gram_poly(self, x: Tensor, degree: int) -> Tensor:
+        p0 = x.new_ones(x.size())
+
+        if degree == 0:
+            return torch.unsqueeze(p0, dim=-1)
+
+        p1 = x
+        grams_basis = [p0, p1]
+
+        for i in range(2, degree + 1):
+            p2 = x * p1 - self.beta(i - 1, i) * p0
+            grams_basis.append(p2)
+            p0, p1 = p1, p2
+
+        indexes = [
+            i * (degree + 1) + j for i in range(x.shape[1]) for j in range(degree + 1)
+        ]
+
+        grams_basis = torch.concat(grams_basis, dim=1)
+        grams_basis = grams_basis[:, indexes]
+        return grams_basis
+
+    def forward_kag(self, x: Tensor) -> Tensor:
+        # Apply base activation to input and then linear transform with base weights
+        basis = self.base_conv(self.base_activation(x))
+
+        # Normalize x to the range [-1, 1] for stable Legendre polynomial computation
+        x = torch.tanh(x).contiguous()
+
+        if self.dropout:
+            x = self.dropout(x)
+
+        grams_basis = self.base_activation(self.gram_poly(x, self.degree))
+
+        y = self.poly_conv(grams_basis)
+        y = self.base_activation(self.layer_norm(y + basis))
+
+        return y
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.forward_kag(x)
+
+
+class KAGNConv3DLayerV2(KAGNConvNDLayerV2):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int | tuple[int, int, int],
+        stride: int | tuple[int, int, int] = 1,
+        padding: PaddingType | int | tuple[int, int, int] = 0,
+        dilation: int | tuple[int, int, int] = 1,
+        groups: int = 1,
+        degree: int = 3,
+        dropout: float = 0.0,
+        **norm_kwargs,
+    ):
+        super(KAGNConv3DLayerV2, self).__init__(
+            conv_class=nn.Conv3d,
+            norm_class=nn.InstanceNorm3d,
+            conv_w_fun=conv3d,
+            ndim=3,
+            in_channels=in_channels,
+            out_channels=out_channels,
+            degree=degree,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            groups=groups,
+            dropout=dropout,
+            **norm_kwargs,
+        )
+
+
+class KAGNConv2DLayerV2(KAGNConvNDLayerV2):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int | tuple[int, int],
+        stride: int | tuple[int, int] = 1,
+        padding: PaddingType | int | tuple[int, int] = 0,
+        dilation: int | tuple[int, int] = 1,
+        groups: int = 1,
+        degree: int = 3,
+        dropout: float = 0.0,
+        **norm_kwargs,
+    ):
+        super(KAGNConv2DLayerV2, self).__init__(
+            conv_class=nn.Conv2d,
+            norm_class=nn.InstanceNorm2d,
+            conv_w_fun=conv2d,
+            ndim=2,
+            in_channels=in_channels,
+            out_channels=out_channels,
+            degree=degree,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            groups=groups,
+            dropout=dropout,
+            **norm_kwargs,
+        )
+
+
+class KAGNConv1DLayerV2(KAGNConvNDLayerV2):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int,
+        stride: int = 1,
+        padding: PaddingType | int = 0,
+        dilation: int = 1,
+        groups: int = 1,
+        degree: int = 3,
+        dropout: float = 0.0,
+        **norm_kwargs,
+    ):
+        super(KAGNConv1DLayerV2, self).__init__(
+            conv_class=nn.Conv1d,
+            norm_class=nn.InstanceNorm1d,
+            conv_w_fun=conv1d,
+            ndim=1,
+            in_channels=in_channels,
+            out_channels=out_channels,
+            degree=degree,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            groups=groups,
+            dropout=dropout,
+            **norm_kwargs,
+        )
