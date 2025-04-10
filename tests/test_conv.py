fbce29677c100ff4fdb92707bb92c80fae4d92fb
jshn9515 <jshn9510@gmail.com>
2025-04-10 15:39:41 +0800

    Move tests for Bottleneck layers to test_bottleneck.py

--- tests/test_conv.py
+++ tests/test_conv.py
@@ -5,9 +5,6 @@ import torch
 import torch.nn as nn
 
 from torchkan.conv import (
-    BottleNeckReLUKANConv1DLayer,
-    BottleNeckReLUKANConv2DLayer,
-    BottleNeckReLUKANConv3DLayer,
     FastKANConv1DLayer,
     FastKANConv2DLayer,
     FastKANConv3DLayer,
@@ -531,95 +528,6 @@ def test_relukan_conv_3d(dropout: float, groups: int):
         batch_size, in_channels, spatial_dim, spatial_dim, spatial_dim
     )
     net = ReLUKANConv3DLayer(
-        in_channels=in_channels,
-        out_channels=out_channels,
-        kernel_size=kernel_size,
-        stride=1,
-        padding=padding,
-        dilation=1,
-        groups=groups,
-        g=5,
-        k=3,
-        train_ab=True,
-        dropout=dropout,
-    )
-    out = net(input_tensor)
-    assert out.shape == (
-        batch_size,
-        out_channels,
-        spatial_dim,
-        spatial_dim,
-        spatial_dim,
-    )
-
-
-@pytest.mark.parametrize('dropout, groups', itertools.product([0.0, 0.5], [1, 4]))
-def test_bottleneck_relukan_conv_1d(dropout: float, groups: int):
-    batch_size = 6
-    in_channels = 4
-    out_channels = 16
-    spatial_dim = 32
-    kernel_size = 3
-    padding = 'same'
-
-    input_tensor = torch.rand(batch_size, in_channels, spatial_dim)
-    net = BottleNeckReLUKANConv1DLayer(
-        in_channels=in_channels,
-        out_channels=out_channels,
-        kernel_size=kernel_size,
-        stride=1,
-        padding=padding,
-        dilation=1,
-        groups=groups,
-        g=5,
-        k=3,
-        train_ab=True,
-        dropout=dropout,
-    )
-    out = net(input_tensor)
-    assert out.shape == (batch_size, out_channels, spatial_dim)
-
-
-@pytest.mark.parametrize('dropout, groups', itertools.product([0.0, 0.5], [1, 4]))
-def test_bottleneck_relukan_conv_2d(dropout: float, groups: int):
-    batch_size = 6
-    in_channels = 4
-    out_channels = 16
-    spatial_dim = 32
-    kernel_size = 3
-    padding = 'same'
-
-    input_tensor = torch.rand(batch_size, in_channels, spatial_dim, spatial_dim)
-    net = BottleNeckReLUKANConv2DLayer(
-        in_channels=in_channels,
-        out_channels=out_channels,
-        kernel_size=kernel_size,
-        stride=1,
-        padding=padding,
-        dilation=1,
-        groups=groups,
-        g=5,
-        k=3,
-        train_ab=True,
-        dropout=dropout,
-    )
-    out = net(input_tensor)
-    assert out.shape == (batch_size, out_channels, spatial_dim, spatial_dim)
-
-
-@pytest.mark.parametrize('dropout, groups', itertools.product([0.0, 0.5], [1, 4]))
-def test_bottleneck_relukan_conv_3d(dropout: float, groups: int):
-    batch_size = 6
-    in_channels = 4
-    out_channels = 16
-    spatial_dim = 32
-    kernel_size = 3
-    padding = 'same'
-
-    input_tensor = torch.rand(
-        batch_size, in_channels, spatial_dim, spatial_dim, spatial_dim
-    )
-    net = BottleNeckReLUKANConv3DLayer(
         in_channels=in_channels,
         out_channels=out_channels,
         kernel_size=kernel_size,
