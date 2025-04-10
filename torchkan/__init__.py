fcc173dfef67ba7503a638df2668fde564e6349b
jshn9515 <jshn9510@gmail.com>
2025-04-10 15:49:30 +0800

    Add get_all_kan_layers function

--- torchkan/__init__.py
+++ torchkan/__init__.py
@@ -1,2 +1,12 @@
+import torch.nn as nn
+
 from .conv import *  # noqa: F403
 from .linear import *  # noqa: F403
+
+
+def get_all_kan_layers() -> list[str]:
+    return [
+        name
+        for name, obj in globals().items()
+        if isinstance(obj, type) and issubclass(obj, nn.Module)
+    ]
