1dad600187ed6040480ddbbfd49fb0642fe6feb1
jshn9515 <jshn9510@gmail.com>
2025-08-31 17:22:49 -0400

    Include U2KAN and DenseKAN in module init file

--- torchkan/models/__init__.py
+++ torchkan/models/__init__.py
@@ -1,2 +1,4 @@
 # ruff: noqa: I001
 from .baselines import *  # noqa: F403
+from .densekanet import DenseKANet as DenseKANet, TinyDenseKANet as TinyDenseKANet
+from .u2kanet import U2KANet as U2KANet, TinyU2KANet as TinyU2KANet
