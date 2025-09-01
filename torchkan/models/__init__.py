cffe7b5bf5c6b8d565b50187bc90ff43ab9ba36f
jshn9515 <jshn9510@gmail.com>
2025-09-01 15:52:10 -0400

    Include ResKANet in module init file

--- torchkan/models/__init__.py
+++ torchkan/models/__init__.py
@@ -1,4 +1,5 @@
 # ruff: noqa: I001
 from .baselines import *  # noqa: F403
 from .densekanet import DenseKANet as DenseKANet, TinyDenseKANet as TinyDenseKANet
+from .reskanet import ResKANet as ResKANet, MoEResKANet as MoEResKANet
 from .u2kanet import U2KANet as U2KANet, TinyU2KANet as TinyU2KANet
