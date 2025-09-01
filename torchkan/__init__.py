f1383c31d899f78136663e8ade35df3d1a8361b3
jshn9515 <jshn9510@gmail.com>
2025-09-01 15:41:45 -0400

    Only import nn.Module in init file

--- torchkan/__init__.py
+++ torchkan/__init__.py
@@ -1,4 +1,4 @@
-import torch.nn as nn
+from torch.nn import Module
 
 from .conv import *  # noqa: F403
 from .linear import *  # noqa: F403
@@ -8,5 +8,5 @@ def get_all_kan_layers() -> list[str]:
     return [
         name
         for name, obj in globals().items()
-        if isinstance(obj, type) and issubclass(obj, nn.Module)
+        if isinstance(obj, type) and issubclass(obj, Module)
     ]
