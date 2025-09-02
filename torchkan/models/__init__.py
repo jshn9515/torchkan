9d1d7d963b923c540f8694f7179f6ab053326d31
jshn9515 <jshn9510@gmail.com>
2025-09-02 18:03:41 -0400

    Include UKAN in module init file

--- torchkan/models/__init__.py
+++ torchkan/models/__init__.py
@@ -3,3 +3,4 @@ from .baselines import *  # noqa: F403
 from .densekanet import DenseKANet as DenseKANet, TinyDenseKANet as TinyDenseKANet
 from .reskanet import ResKANet as ResKANet, MoEResKANet as MoEResKANet
 from .u2kanet import U2KANet as U2KANet, TinyU2KANet as TinyU2KANet
+from .ukanet import UKANet as UKANet, UKAGNet as UKAGNet
